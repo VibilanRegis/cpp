8de58438a242108f029d0ee0d0de5ab4e348bdff
VibilanRegis <168193489+VibilanRegis@users.noreply.github.com>
2025-08-14 09:53:14 +0000

    added a comment

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -1,5 +1,5 @@
 // Complete Guide to C++ Programming Foundations
-// Exercise 00_03
+// Exercise 00_03 and modification
 // Using the Exercise Files in GitHub Codespaces, by Eduardo Corpeño 
 
 #include <iostream>
