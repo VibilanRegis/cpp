bb83ffff1c1e57a4393c9e6d2f0c6744284af80d
VibilanRegis <168193489+VibilanRegis@users.noreply.github.com>
2025-08-14 11:28:31 +0000

    hello world with comments

--- src/Ch01/01_02b/CodeDemoVibilan.cpp
+++ src/Ch01/01_02b/CodeDemoVibilan.cpp
@@ -1,13 +1,20 @@
-// hello world in cpp - 14_Aug_2025
+// // hello world in cpp - 14_Aug_2025
 
-#include<iostream>
+// #include<iostream>
 
-int main(){
-    std::cout<< "Hello World"<<std::endl;
+// int main(){
+//     std::cout<< "Hello World"<<std::endl;
 
 
-    std::cout<<std::endl<<std::endl;
+//     std::cout<<std::endl<<std::endl;
 
-    return 0;
+//     return 0;
 
-}
+// }
+
+
+//DONT USE 2 MAIN
+
+//This includes both CodeDemo.cpp and CodeDemoVibilan.cpp, and both files contain a main() function, which causes a linker error:
+
+// multiple definition of `main`
